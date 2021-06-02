287ec8de6a1c753eb0a70251d8b54d1c4cae53fc
luca.dibello@student.supsi.ch <luca.dibello@student.supsi.ch>
2021-06-02 18:39:58 +0200

    New dynamic gallery component

---
+++ client/components/Gallery.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="gallery">
+    <div v-for="(image, i) in images" :key="i" class="image-container">
+      <b-img
+        class="image img-fluid shadow-lg bg-body rounded"
+        :src="image.url"
+        :alt="image.alt"
+        @click="$emit('update:gallery',i)"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Gallery',
+  props: {
+    images: {
+      type: Array,
+      required: true
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .gallery {
+    display: grid;
+    grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
+    grid-auto-rows: auto;
+    text-align: center;
+    overflow: hidden;
+  }
+  .gallery img {
+    cursor: pointer;
+    max-height: 40vh;
+  }
+
+  .image-container {
+    margin-top:2vh;
+  }
+</style>
