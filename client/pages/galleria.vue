f5a05f713387a4fa8c4c7f4f987090da574a608a
luca.dibello@student.supsi.ch <luca.dibello@student.supsi.ch>
2021-06-02 18:41:16 +0200

    New pages

---
+++ client/pages/galleria.vue
@@ -0,0 +1,60 @@
+<template>
+  <section id="gallery">
+    <b-container fluid>
+      <h2 class="display-5">
+        <span class="title">Galleria</span>
+        <b-badge variant="success" class="mx-3">
+          {{ images.length }} Foto
+        </b-badge>
+      </h2>
+      <!-- Show only a row -->
+      <p>Clicca su una foto per ingrandirla</p>
+
+      <!-- Show gallery -->
+      <gallery :images="images" @update:gallery="index = $event" />
+      <client-only>
+        <!-- Show lightbox if clicked -->
+        <vgs :images="images" :index="index" @close="index = null" />
+      </client-only>
+    </b-container>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'Galleria',
+  layout: 'default',
+  data: () => ({
+    images: [
+      require('assets/img/gallery/foto1.jpg'),
+      require('assets/img/gallery/foto2.jpg'),
+      require('assets/img/gallery/foto3.jpg'),
+      require('assets/img/gallery/foto4.jpg'),
+      require('assets/img/gallery/foto5.jpg'),
+      require('assets/img/gallery/foto6.jpeg'),
+      require('assets/img/gallery/foto7.jpeg'),
+      require('assets/img/gallery/foto8.jpeg'),
+      require('assets/img/gallery/foto9.jpeg'),
+      require('assets/img/gallery/foto10.jpg')
+    ],
+    index: null
+  }),
+  head: () => ({
+    title: 'Galleria',
+    meta: [{
+      hid: 'description',
+      name: 'description',
+      content: 'Album fotografico digitale del Ristorante Al Torchio Antico'
+    }, {
+      hid: 'og:description',
+      name: 'og:description',
+      content: 'Album fotografico digitale del Ristorante Al Torchio Antico'
+    },
+    {
+      hid: 'og:title',
+      name: 'og:title',
+      content: 'Galleria'
+    }]
+  })
+}
+</script>
---
+++ client/pages/orari.vue
@@ -0,0 +1,109 @@
+<template>
+  <section>
+    <b-container fluid>
+      <client-only>
+        <restourant-status :hours="hours" :show-button="false" :show-hours="false" />
+      </client-only>
+      <h2 class="display-5 title">
+        Orario settimanale:
+      </h2>
+      <b-table
+        hover
+        striped
+        responsive
+        small
+        class="mt-3 text-center"
+        variant="dark"
+        :items="hours"
+        :fields="hoursField"
+      />
+    </b-container>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'Orari',
+  data: () => ({
+    hours: [
+      { // SUNDAY
+        name: 'Domenica',
+        isOpen: true,
+        open: '09:00',
+        closes: '23:00'
+      },
+      { // MONDAY
+        name: 'Lunedì',
+        isOpen: false,
+        open: 'Chiuso',
+        closes: 'Chiuso',
+        _rowVariant: 'danger'
+      },
+      { // TUESDAY
+        name: 'Martedì',
+        isOpen: true,
+        open: '17:00',
+        closes: '23:00'
+      },
+      { // WEDNESDAY
+        name: 'Mercoledì',
+        isOpen: true,
+        open: '17:00',
+        closes: '23:00'
+      },
+      { // THURSDAY
+        name: 'Giovedì',
+        isOpen: true,
+        open: '17:00',
+        closes: '23:00'
+      },
+      { // FRIDAY
+        name: 'Venerdì',
+        isOpen: true,
+        open: '09:00',
+        closes: '23:00'
+      },
+      { // SATURDAY
+        name: 'Sabato',
+        isOpen: true,
+        open: '09:00',
+        closes: '23:00'
+      }
+    ],
+    hoursField: [
+      {
+        key: 'name',
+        label: 'Giorno',
+        sortable: false
+      },
+      {
+        key: 'open',
+        label: 'Orario apertura',
+        sortable: true
+      },
+      {
+        key: 'closes',
+        label: 'Orario chiusura',
+        sortable: true
+      }
+    ]
+  }),
+  head: () => ({
+    title: 'Orari',
+    meta: [{
+      hid: 'description',
+      name: 'description',
+      content: 'Orari settimanali ristorante Al Torchio Antico'
+    }, {
+      hid: 'og:description',
+      name: 'og:description',
+      content: 'Orari settimanali ristorante Al Torchio Antico'
+    },
+    {
+      hid: 'og:title',
+      name: 'og:title',
+      content: 'Orari'
+    }]
+  })
+}
+</script>
